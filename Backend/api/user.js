ab4fb26e9bd4e146c091292cbd840423bd1ac7df
BaptFr <baptistesalazar@gmail.com>
2025-01-28 19:24:57 +0100

    modifs for depl

--- Backend/api/user.js
+++
@@ -1,9 +0,0 @@
-import userCtrl from '../controllers/users.controller';
-
-export default function handler(req, res) {
-    if (req.method === 'POST' && req.url === '/login') {
-        userCtrl.login(req, res);
-    } else {
-        res.status(405).json({ message: 'Method Not Allowed' });
-    }
-}
